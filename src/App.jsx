1e438b4cbac81d98bd321760474a83e5d1cae8b1
Y04ash <yashranbhare2020@gmail.com>
2025-06-01 19:48:20 +0530

    fixed app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,3 @@
-// import React, { useState, useEffect, createContext } from "react";
-// import "./css/App.css"; // App general styles
-// import { useResultContext } from "./context/ResultContext";
-// import ReactDOM from 'react-dom/client';
-// import Sidebar from "./components/Sidebar";
-// import VisualizeLanding from "./pages/VisualizeLanding";
-// import CustomerClustered from "./pages/CustomerClustered";
-// import Apriori from "./pages/Apriori";
-// import RFMPage from "./pages/RFMPage";
-// import { BrowserRouter } from 'react-router-dom';
-// // import * as ResultContext from './context/ResultContext';
-// import { useNavigate } from 'react-router-dom';
-// import UploadPage from "./pages/Upload";
-// import Home from "./pages/Home";
-// import { ResultProvider } from "./context/ResultContext";
-// function App() {
-
-//   const { data,setData,isSidebarOpen,setIsSidebarOpen } = useResultContext();
-//   const navigate = useNavigate();
-
-
-//   const toggleSidebar = () => {
-//     setIsSidebarOpen(!isSidebarOpen);
-//   };
-
-
-//   return (
-//     // <BrowserRouter>
-//       <ResultProvider>
-//         <div className="app-container lato-bold">
-//           <Sidebar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
-//           <div className="content">
-//             <Routes>
-//               <Route path="/" element={<Home />} />
-//               <Route path="/upload" element={<UploadPage />} />
-//               <Route path="/visualize" element={<VisualizeLanding />} />
-//               <Route path="/customer-clustered" element={<CustomerClustered />} />
-//               <Route path="/apriori" element={<Apriori />} />
-//               <Route path="/rfm" element={<RFMPage />} />
-//             </Routes>
-//           </div>
-//         </div>
-//       </ResultProvider>)
-//     {/* </BrowserRouter> */}
-    
-// }
-
-// export default App;
 
 
 // src/App.jsx
@@ -54,7 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ResultProvider } from "./context/ResultContext";
 import './index.css'
 import { useResultContext } from './context/ResultContext';
-import "./css/App.css";
+import "./css/app.css";
 import Sidebar from "./components/Sidebar";
 
 import Home from "./pages/Home";
