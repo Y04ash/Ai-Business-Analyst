a4b89f6895f132cfd7af6d9b59f032f93713cc6c
Y04ash <yashranbhare2020@gmail.com>
2025-06-13 23:35:05 +0530

    cors fixed

--- ml/app.py
+++ ml/app.py
@@ -76,7 +76,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173","https://ai-powered-business-analyst.onrender.com"],
+    allow_origins=["http://localhost:5173","https://ai-powered-business-analyst.onrender.com","https://ai-powered-business-analyst.onrender.com/upload"],
     
     allow_credentials=True,
     allow_methods=["*"],
